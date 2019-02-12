f3978145d89a1c504ef42798b70def68067ec42c
vattikondahemanth <hemanth.hemanth180@gmail.com>
2019-02-11 23:58:15 -0800

    Start Changes

--- python3/koans/about_decorating_with_classes.py
+++ python3/koans/about_decorating_with_classes.py
@@ -108,7 +108,7 @@ class AboutDecoratingWithClasses(Koan):
         pass
 
     def test_decorator_with_an_argument(self):
-        self.assertEqual(__, self.count_badly(2))
+        self.assertEqual(5, self.count_badly(2))
         self.assertEqual(__, self.count_badly.__doc__)
 
     def test_documentor_which_already_has_a_docstring(self):
