71d0c9c73c581e7983a9f1d4a87bf7b02a739851
vattikondahemanth <hemanth.hemanth180@gmail.com>
2019-02-12 00:37:54 -0800

    after deletion

--- python3/koans/about_decorating_with_classes.py
+++ python3/koans/about_decorating_with_classes.py
@@ -109,7 +109,7 @@ class AboutDecoratingWithClasses(Koan):
 
     def test_decorator_with_an_argument(self):
         self.assertEqual(5, self.count_badly(2))
-        self.assertEqual(__, self.count_badly.__doc__)
+        self.assertEqual("Increments a value by one. Kind of.", self.count_badly.__doc__)
 
     def test_documentor_which_already_has_a_docstring(self):
         self.assertEqual(__, self.idler.__doc__)
